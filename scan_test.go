16d91d2bb060eba3c65f0c0ab2e4d52a53588308
pre <preimmortal@gmail.com>
2019-10-06 15:29:33 -0700

    remove scanner from testing

--- scan_test.go
+++ scan_test.go
@@ -1,44 +1,45 @@
-package smarthome
-
-import (
-	"testing"
-)
-
-func TestScan(t *testing.T) {
-	db := &Database{}
-	s := Scan{db}
-	local_ip := "192.168.1.0/24"
-	t.Log("Scanning ", local_ip)
-	result, err := s.Scan(local_ip)
-	if err != nil {
-		t.Fatalf("Could not complete scan: %v", err)
-	}
-
-	for _, host := range result.Hosts {
-		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
-			continue
-		}
-		t.Logf("Host %q: \n", host.Addresses[0])
-
-		for _, port := range host.Ports {
-			t.Logf("\tPort %d/%s %s %s\n", port.ID, port.Protocol, port.State, port.Service.Name)
-		}
-	}
-
-	t.Logf("Nmap done: %d hosts up scanned in %3f seconds\n", len(result.Hosts), result.Stats.Finished.Elapsed)
-}
-
-func TestFindDefaultRoute(t *testing.T) {
-	db := &Database{}
-	s := Scan{db}
-	t.Log("Finding Default Route")
-	ip, err := s.findDefaultRoute()
-	if err != nil {
-		t.Fatalf("Could not find default route: %v", err)
-	}
-	t.Log("Found IP: ", ip.String())
-
-}
+//TODO: Will not work in CI network
+//package smarthome
+//
+//import (
+//	"testing"
+//)
+//
+//func TestScan(t *testing.T) {
+//	db := &Database{}
+//	s := Scan{db}
+//	local_ip := "192.168.1.0/24"
+//	t.Log("Scanning ", local_ip)
+//	result, err := s.Scan(local_ip)
+//	if err != nil {
+//		t.Fatalf("Could not complete scan: %v", err)
+//	}
+//
+//	for _, host := range result.Hosts {
+//		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+//			continue
+//		}
+//		t.Logf("Host %q: \n", host.Addresses[0])
+//
+//		for _, port := range host.Ports {
+//			t.Logf("\tPort %d/%s %s %s\n", port.ID, port.Protocol, port.State, port.Service.Name)
+//		}
+//	}
+//
+//	t.Logf("Nmap done: %d hosts up scanned in %3f seconds\n", len(result.Hosts), result.Stats.Finished.Elapsed)
+//}
+//
+//func TestFindDefaultRoute(t *testing.T) {
+//	db := &Database{}
+//	s := Scan{db}
+//	t.Log("Finding Default Route")
+//	ip, err := s.findDefaultRoute()
+//	if err != nil {
+//		t.Fatalf("Could not find default route: %v", err)
+//	}
+//	t.Log("Found IP: ", ip.String())
+//
+//}
 
 //func TestStart(t *testing.T) {
 //	db := &Database{}
