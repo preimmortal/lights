019ff4292a79c55e0f9e3bb54e0504a121ab26b1
pre <preimmortal@gmail.com>
2019-10-09 22:29:49 -0700

    add extra fields to frontend device display

--- frontend/smarthome/src/components/Devices.js
+++ frontend/smarthome/src/components/Devices.js
@@ -8,10 +8,16 @@ class Devices extends Component {
         super(props);
         this.state = {
             devices: [],
+            // devices {
+            //     name  : ""
+            //     ip    : ""
+            //     alias : ""
+            //     state : ""
+            // }
         }
     }
     componentDidMount() {
-        fetch('http://192.168.1.203:8081/devices')
+        fetch('http://localhost:8081/devices')
         .then(res => res.json())
         .then((data) => {
             this.setState({ devices: data })
@@ -23,31 +29,35 @@ class Devices extends Component {
         return (
             <div>
                 <h2>Found the following list of devices:</h2>
-                <table class="table">
+                <table className="table">
                     <thead>
-                        <th scope="col">#</th>
-                        <th scope="col">Device Name</th>
-                        <th scope="col">IP Address</th>
-                        <th scope="col">Status</th>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Device Name</th>
+                            <th scope="col">Device Type</th>
+                            <th scope="col">IP Address</th>
+                            <th scope="col">State</th>
+                        </tr>
                     </thead>
                     <tbody>
                             {devices.map((device, index) =>
-                                <tr>
+                                <tr  key={index+1}>
                                     <th scope="row"><a href={'/devices/' + device.ip}>{index+1}</a></th>
-                                    <td key={index+1}>
+                                    <td>
+                                        <a href={'/devices/' + device.ip}>{device.alias}</a>
+                                    </td>
+                                    <td>
                                         <a href={'/devices/' + device.ip}>{device.name}</a>
                                     </td>
                                     <td>
                                         <a href={'/devices/' + device.ip}>{device.ip}</a>
                                     </td>
                                     <td>
-                                        <a href={'/devices/' + device.ip}>OFF</a>
+                                        <a href={'/devices/' + device.ip}>{device.state}</a>
                                     </td>
                                 </tr>
                             )}
                     </tbody>
-                <ul>
-                </ul>
                 </table>
             </div>
         )
--- frontend/smarthome/src/components/Navbar.js
+++ frontend/smarthome/src/components/Navbar.js
@@ -5,17 +5,17 @@ import '../slate/bootstrap.min.css';
 class Navbar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a class="navbar-brand" href="/">Smarthome</a>
-                <div class="collapse navbar-collapse" id="navbarColor01">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <NavLink to="/" className="nav-link">Home <span class="sr-only">(current)</span></NavLink>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a className="navbar-brand" href="/">Smarthome</a>
+                <div className="collapse navbar-collapse" id="navbarColor01">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <NavLink to="/" className="nav-link">Home <span className="sr-only">(current)</span></NavLink>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <NavLink to="/devices" className="nav-link">Devices</NavLink>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <NavLink to="/about" className="nav-link">About</NavLink>
                         </li>
                     </ul>
