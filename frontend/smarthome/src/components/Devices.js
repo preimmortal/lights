9004c65ab4b8eb194e738a65dba022041fb673d5
pre <preimmortal@gmail.com>
2019-10-09 20:51:27 -0700

    fix navbar, add devices api call

--- frontend/smarthome/src/components/Devices.js
+++ frontend/smarthome/src/components/Devices.js
@@ -4,18 +4,51 @@ import '../slate/bootstrap.min.css';
 
 
 class Devices extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            devices: [],
+        }
+    }
     componentDidMount() {
-        fetch('192.168.1.203:8081/devices')
+        fetch('http://192.168.1.203:8081/devices')
         .then(res => res.json())
         .then((data) => {
-            this.setState({ contacts: data })
+            this.setState({ devices: data })
         })
         .catch(console.log)
     }
     render() {
+        const {devices} = this.state;
         return (
             <div>
-              <p1>Hello from devices</p1>
+                <h2>Found the following list of devices:</h2>
+                <table class="table">
+                    <thead>
+                        <th scope="col">#</th>
+                        <th scope="col">Device Name</th>
+                        <th scope="col">IP Address</th>
+                        <th scope="col">Status</th>
+                    </thead>
+                    <tbody>
+                            {devices.map((device, index) =>
+                                <tr>
+                                    <th scope="row"><a href={'/devices/' + device.ip}>{index+1}</a></th>
+                                    <td key={index+1}>
+                                        <a href={'/devices/' + device.ip}>{device.name}</a>
+                                    </td>
+                                    <td>
+                                        <a href={'/devices/' + device.ip}>{device.ip}</a>
+                                    </td>
+                                    <td>
+                                        <a href={'/devices/' + device.ip}>OFF</a>
+                                    </td>
+                                </tr>
+                            )}
+                    </tbody>
+                <ul>
+                </ul>
+                </table>
             </div>
         )
     }
--- frontend/smarthome/src/components/Navbar.js
+++ frontend/smarthome/src/components/Navbar.js
@@ -4,32 +4,24 @@ import '../slate/bootstrap.min.css';
 
 class Navbar extends Component {
     render() {
-      return (
-          <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="/">Smarthome</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          
-            <div class="collapse navbar-collapse" id="navbarColor01">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <NavLink to="/" class="nav-link">Home <span class="sr-only">(current)</span></NavLink>
-                </li>
-                <li class="nav-item">
-                  <NavLink to="/devices" class="nav-link">Devices</NavLink>
-                </li>
-                <li class="nav-item">
-                  <NavLink to="/about" class="nav-link">About</NavLink>
-                </li>
-              </ul>
-              <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search"></input>
-                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-              </form>
-            </div>
-          </nav>
-      )
+        return (
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a class="navbar-brand" href="/">Smarthome</a>
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <NavLink to="/" className="nav-link">Home <span class="sr-only">(current)</span></NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink to="/devices" className="nav-link">Devices</NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink to="/about" className="nav-link">About</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
     }
 }
 
