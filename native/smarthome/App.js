57575af9eece8f93764aa7782b22183ef8bbbca7
pre <preimmortal@gmail.com>
2019-11-08 10:02:48 -0800

    moving to native apps

--- native/smarthome/App.js
+++ native/smarthome/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import Devices from './components/Device'
+import {StyleSheet, Text, View} from 'react-native';
+import Devices from './components/Device';
 
 export default function App() {
   return (
